b50b0301ddbaf5602dd99609eff273066e0a00fb
Letocart Thomas <tom.96@live.fr>
2021-03-25 14:00:22 +0100

    Sécurité

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const sauces = require ('./routes/sauce.js');
 // On fait appel à la base de données
 const mongoose = require('mongoose');
 
-// Connexion à la base de données
-mongoose.connect('mongodb+srv://TLetocart:toto02toto@cluster0.1qgxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+// Connexion à la base de données avec une variable d'environnement pour proteger les infos de connexions a la base de donnée
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_URI,
   { useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
@@ -33,3 +36,24 @@ app.use('/api/auth', signup);
 app.use('/api/sauces', sauces);
 
 module.exports = app;
+
+// Sécurité
+
+// Configure, les en-têtes HTTP pour protéger de certaines vulnérabilités
+const helmet = require('helmet'); 
+const session = require('cookie-session');
+const nocache = require('nocache')
+
+app.use(helmet());
+app.use(nocache()); // Empêche la mise en cache
+
+// Eviter les erreurs CORS pour ouvrir les requêtes
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Content-Security-Policy', "default-src 'self'");
+  next();
+});
+
